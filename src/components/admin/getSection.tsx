58d8d29f5be9c930e166e68412a8d75045a40d96
minhazpro3 <minhazpro1@gmail.com>
2023-06-16 00:10:50 +0600

    alert message

--- src/components/admin/getSection.tsx
+++ src/components/admin/getSection.tsx
@@ -1,13 +1,14 @@
 import axiosInstance from "@/lib/axios";
 import { useEffect, useState } from "react";
-import {RiDeleteBin5Line} from "react-icons/ri";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import toast, { Toaster } from 'react-hot-toast';
 
-const GetSection = ({data, setData}: any) => {
-  
+const GetSection = ({ data, setData }: any) => {
+  const [onButton, setOnButton] = useState(true)
+  const [onId, setOnId] = useState("")
 
   const getData = async () => {
     const response = await axiosInstance(`/api/v1/sections`)
-
     setData((response as any).data?.data)
   }
 
@@ -15,30 +16,44 @@ const GetSection = ({data, setData}: any) => {
     getData()
   }, []);
 
-const handleDelete = async (id: any) => {
-  const deleteData = await axiosInstance.delete(`/api/v1/sections/${id}`)
-  if (deleteData.status == 201) {
-    const newData = data.filter((e: any) => e._id !== id)
-    setData(newData);
+  const handleDelete = async (id: any) => {
+    setOnButton(false)
+    setOnId(id)
+    const deleteData = await axiosInstance.delete(`/api/v1/sections/${id}`)
+    if (deleteData.status == 201) {
+      const newData = data.filter((e: any) => e._id !== id)
+      setData(newData);
+      toast.success('Successfully Delete Data!')
+      setOnButton(true)
+    } else {
+      toast.error('Something wrong!')
+      setOnButton(true)
+    }
+
   }
-  
-}
 
   return (
     <>
       {(data as any)?.map((sdata: any, index: any) => (
-          <div key={sdata._id} className="max-w-sm mt-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div onClick={() => handleDelete((sdata as any)._id)} className="float-right text-red-700 cursor-pointer"><RiDeleteBin5Line /></div>
-            <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              {sdata.name}
-            </h5>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              {sdata.topic}
-            </p>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {sdata.documents}
-            </p>
-          </div>
+        <div key={sdata._id} className="max-w-sm mt-4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <Toaster
+            position="top-right"
+            reverseOrder={true}
+          />
+          <div onClick={() => handleDelete((sdata as any)._id)} className="float-right text-red-700 cursor-pointer">{!onButton && onId === sdata._id ? <button className="float-right bg-blue-400 text-red-700 p-2 " disabled={true}>  <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+          </svg></button> : <RiDeleteBin5Line />}</div>
+          <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {sdata.name}
+          </h5>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            {sdata.topic}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {sdata.documents}
+          </p>
+        </div>
       ))}
     </>
   );
--- src/components/admin/sections.tsx
+++ src/components/admin/sections.tsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 import GetSection from "./getSection";
 const languagesData = ["Python", "Javascript", "Go", "C++", "C#", "C"];
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function SectionsPage() {
+  const [onButton, setOnButton] = useState(true)
   const [getData, setGetData] = useState<any>([]);
   const [data, setData] = useState({
     topic: "Python",
@@ -19,18 +22,33 @@ export default function SectionsPage() {
     });
   };
 
+
+
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setOnButton(false)
     const response = await axios.post(`/api/v1/sections`, {
       topic: data.topic.toLowerCase(),
       name: `${data.topic} ${data.name}`,
       order: Number(data.order),
     });
-    if(response.status == 201){
+    if (response.status == 201) {
       setGetData([...getData, (response as any).data?.data])
+      toast.success('Successfully Post Data!')
+      setOnButton(true)
+    } else {
+      toast.error("Something went wrong!")
+      setOnButton(true)
     }
-    console.log(response);
+
   };
+
+
+
+
+
+
   return (
     <div className="grid grid-cols-12 gap-3">
       <form
@@ -38,6 +56,10 @@ export default function SectionsPage() {
         className="col-span-8 grid grid-cols-6 gap-2"
       >
         <fieldset className="col-span-5">
+          <Toaster
+            position="top-right"
+            reverseOrder={true}
+          />
           <div className="grid grid-cols-2 gap-4">
             <select
               required
@@ -75,16 +97,30 @@ export default function SectionsPage() {
           />
         </fieldset>
         <div className="col-span-1">
-          <button
-            type="submit"
-            className="py-4 px-4 bg-emerald-700 text-gray-200 rounded border-2 border-gray-200 hover:text-emerald-700 hover:border-emerald-700 hover:bg-gray-200"
-          >
-            <AiOutlineAppstoreAdd />
-          </button>
+          {onButton ?
+            <button
+              type="submit"
+              className="py-4 px-4 bg-emerald-700 text-gray-200 rounded border-2 border-gray-200 hover:text-emerald-700 hover:border-emerald-700 hover:bg-emerald-400"
+            >
+              <AiOutlineAppstoreAdd />
+            </button>
+
+            :
+            <button
+              disabled={true}
+              type="submit"
+              className="py-4 px-4 bg-emerald-700 text-gray-200 rounded border-2 border-gray-200 hover:text-emerald-700 hover:border-emerald-700 hover:bg-emerald-400"
+            >
+              <svg aria-hidden="true" role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+              </svg>
+            </button>}
         </div>
       </form>
+
       <div className="col-span-4">
-       <GetSection data={getData} setData={setGetData} />
+        <GetSection data={getData} setData={setGetData} />
       </div>
     </div>
   );
