24fbc02fb034c0394eaeea20046fafea7eedd586
minhazpro3 <minhazpro1@gmail.com>
2023-06-21 22:43:37 +0600

    api handle and add new input

--- src/components/admin/markdownTextarea.tsx
+++ src/components/admin/markdownTextarea.tsx
@@ -1,5 +1,6 @@
 import axios from "@/lib/axios";
 import { useState } from "react";
+import toast, { Toaster } from 'react-hot-toast';
 import TextareaAutosize from "react-textarea-autosize";
 
 interface PropsTypes {
@@ -16,6 +17,7 @@ interface DataTypes {
   meta_description?: string;
   topic?: string;
   meta_keywords: string;
+  slug?: string;
 }
 
 export default function MarkdownTextarea({
@@ -29,6 +31,7 @@ export default function MarkdownTextarea({
 
   const handleChange = (e: any) => {
     setMarkdownContent(e.target.value);
+
   };
 
   const handleFieldChange = (e: any) => {
@@ -36,21 +39,35 @@ export default function MarkdownTextarea({
       ...data,
       [e.target.name]: e.target.value,
     });
+
   };
 
-  const { name, meta_title, meta_description, topic, meta_keywords } =
+  const { name, meta_title, meta_description, topic, meta_keywords, slug } =
     data || {};
   const handleSubmitDoc = async (e: any) => {
-    const response = await axios.post("/api/v1/doc", {
-      ...data,
-      content: markdownContent,
-    });
-    console.log(response);
+
+    if (name&&meta_title&&meta_description&&topic&&meta_keywords&&slug&&markdownContent) {
+      const response = await axios.post("/api/v1/doc", {
+        ...data,
+        content: markdownContent,
+      });
+      if (response.status===201) {
+        toast.success('Successfully Post Markdown!')
+      } else {
+        toast.error("something went wrong")
+      }
+    }else {
+      toast.error("Please input data")
+    }
+
   };
   return (
     <div>
-
-        <h4 className="my-2 font-bold text-3xl">Share you pure <span className=" text-sky-700 font-extrabold  ">creativity</span></h4>
+      <Toaster
+        position="top-right"
+        reverseOrder={true}
+      />
+      <h4 className="my-2 font-bold text-3xl">Share you pure <span className=" text-sky-700 font-extrabold   uppercase">creativity</span></h4>
       <div className="  p-2 mt-2 mb-6 border-2 border-amber-600">
         <button
           onClick={() => setFields(!fields)}
@@ -73,114 +90,114 @@ export default function MarkdownTextarea({
         </button>
       </div>
 
-<div className={`${!fields? "flex":"block"}`}>
-
-  
-  {fields && (
-        <div >
-          <fieldset className="mb-6 px-2">
-           <div className="flex gap-x-2">
-             {/* Section or Doc sort Name */}
-             <input
-              required
-              name="name"
-              value={name}
-              onChange={handleFieldChange}
-              type="text"
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400"
-              placeholder="Doc sort Name"
-            />
-            {/* Select languages */}
-            <select
-              required
-              onChange={handleFieldChange}
-              name="topic"
-              value={topic}
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
-            >
-              <option className="py-2" value="Python">
-                Python
-              </option>
-              <option className="py-2" value="Javascript">
-                Javascript
-              </option>
-              <option className="py-2" value="Typescript">
-                Typescript
-              </option>
-              <option className="py-2" value="Golang">
-                Golang
-              </option>
-              <option className="py-2" value="C++">
-                C++
-              </option>
-              <option className="py-2" value="C#">
-                C#
-              </option>
-              <option className="py-2" value="C">
-                C
-              </option>
-            </select>
-           </div>
-          </fieldset>
-          {/* SEO */}
-          <fieldset className="mb-6 pb-0 px-2">
-            <input
-              required
-              type="text"
-              name="meta_title"
-              value={meta_title}
-              onChange={handleFieldChange}
-              placeholder="Enter you meta title"
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
-            />
-            <input
-              required
-              type="text"
-              name="slug"
-              value={meta_title}
-              onChange={handleFieldChange}
-              placeholder="Enter you slug ID"
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
-            />
-            <textarea
-              required
-              name="meta_description"
-              value={meta_description}
-              rows={5}
-              onChange={handleFieldChange}
-              placeholder="Enter you meta description"
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
-            ></textarea>
-            <textarea
-              required
-              name="meta_keywords"
-              value={meta_keywords}
-              rows={5}
-              onChange={handleFieldChange}
-              placeholder="Enter you meta keywords"
-              className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
-            ></textarea>
-          </fieldset>
-        </div>
-      )}
+      <div className={`${!fields ? "flex" : "block"}`}>
 
 
+        {fields && (
+          <div >
+            <fieldset className="mb-6 px-2">
+              <div className="flex gap-x-2">
+                {/* Section or Doc sort Name */}
+                <input
+                  required
+                  name="name"
+                  value={name}
+                  onChange={handleFieldChange}
+                  type="text"
+                  className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400"
+                  placeholder="Doc sort Name"
+                />
+                {/* Select languages */}
+                <select
+                  required
+                  onChange={handleFieldChange}
+                  name="topic"
+                  value={topic}
+                  className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
+                >
+                  <option className="py-2" value="Python">
+                    Python
+                  </option>
+                  <option className="py-2" value="Javascript">
+                    Javascript
+                  </option>
+                  <option className="py-2" value="Typescript">
+                    Typescript
+                  </option>
+                  <option className="py-2" value="Golang">
+                    Golang
+                  </option>
+                  <option className="py-2" value="C++">
+                    C++
+                  </option>
+                  <option className="py-2" value="C#">
+                    C#
+                  </option>
+                  <option className="py-2" value="C">
+                    C
+                  </option>
+                </select>
+              </div>
+            </fieldset>
+            {/* SEO */}
+            <fieldset className="mb-6 pb-0 px-2">
+              <input
+                required
+                type="text"
+                name="meta_title"
+                value={meta_title}
+                onChange={handleFieldChange}
+                placeholder="Enter you meta title"
+                className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
+              />
+              <input
+                required
+                type="text"
+                name="slug"
+                value={slug}
+                onChange={handleFieldChange}
+                placeholder="Enter you slug "
+                className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
+              />
+              <textarea
+                required
+                name="meta_description"
+                value={meta_description}
+                rows={5}
+                onChange={handleFieldChange}
+                placeholder="Enter you meta description"
+                className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
+              ></textarea>
+              <textarea
+                required
+                name="meta_keywords"
+                value={meta_keywords}
+                rows={5}
+                onChange={handleFieldChange}
+                placeholder="Enter you meta keywords"
+                className="border-2 outline-none border-sky-600 w-full py-1 px-2  rounded-md bg-slate-950  focus:underline-none focus:border-sky-400 "
+              ></textarea>
+            </fieldset>
+          </div>
+        )}
 
 
-      {/* markdown writer */}
-      {!preview && (
-        <TextareaAutosize
-          className="textbox py-3 px-2 min-h-[300px] w-full bg-[#333] border-2   hover:border-[#666] text-lg focus:outline-none"
-          value={markdownContent}
-          onChange={handleChange}
-          placeholder="Start typing your Markdown here..."
-        />
-      )}
-  
-  
-</div>
 
-   
+
+        {/* markdown writer */}
+        {!preview && (
+          <TextareaAutosize
+            className="textbox py-3 px-2 min-h-[300px] w-full bg-[#333] border-2   hover:border-[#666] text-lg focus:outline-none"
+            value={markdownContent}
+            onChange={handleChange}
+            placeholder="Start typing your Markdown here..."
+          />
+        )}
+
+
+      </div>
+
+
 
 
     </div>
